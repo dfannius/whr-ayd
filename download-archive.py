06343207d7389f429357a09cdeaa0f15a9c108cc
Dan Schmidt <dfan@dfan.org>
2018-05-02 08:29:06 -0400

    Put season files in correct directory

--- download-archive.py
+++ download-archive.py
@@ -1,8 +1,13 @@
+import os
 import urllib.request
+
+dir = "seasons"
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
 seasons = range(8, 22)
 for season in seasons:
     season_url = "https://ayd.yunguseng.com/season{}/overview.html".format(season)
-    season_file = "season_{:02d}.html".format(season)
-    print(season_url)
+    season_file = "{}/season_{:02d}.html".format(dir, season)
+    print("{} -> {}".format(season_url, season_file))
     urllib.request.urlretrieve(season_url, season_file)
