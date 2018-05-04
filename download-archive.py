b39ca9cc19c9e9f8ec6f0b90ab10ac49735d2b57
Dan Schmidt <dfan@dfan.org>
2018-05-04 08:31:47 -0400

    Download individual season pages

--- download-archive.py
+++ download-archive.py
@@ -1,11 +1,16 @@
 import os
+import sys
 import urllib.request
 
 dir = "seasons"
 if not os.path.exists(dir):
     os.mkdir(dir)
 
-seasons = range(8, 22)
+if len(sys.argv) > 1:
+    seasons = [int(sys.argv[1])]
+else:
+    seasons = range(8, 22)
+
 for season in seasons:
     season_url = "https://ayd.yunguseng.com/season{}/overview.html".format(season)
     season_file = "{}/season_{:02d}.html".format(dir, season)
