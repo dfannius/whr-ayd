27c72b681a2b4123ca48977ae52b1456bd438a18
Dan Schmidt <dfan@dfan.org>
2018-05-23 17:00:32 -0400

    AYD/EYD

--- download-archive.py
+++ download-archive.py
@@ -2,7 +2,8 @@ import os
 import sys
 import urllib.request
 
-dir = "seasons"
+league = "ayd"
+dir = "{}-seasons".format(league)
 if not os.path.exists(dir):
     os.mkdir(dir)
 
@@ -12,7 +13,7 @@ else:
     seasons = range(8, 22)
 
 for season in seasons:
-    season_url = "https://ayd.yunguseng.com/season{}/overview.html".format(season)
+    season_url = "https://{}.yunguseng.com/season{}/overview.html".format(league,season)
     season_file = "{}/season_{:02d}.html".format(dir, season)
     print("{} -> {}".format(season_url, season_file))
     urllib.request.urlretrieve(season_url, season_file)
